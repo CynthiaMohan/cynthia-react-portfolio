ae909d00d45004c6825ebbe0dc845992ac17286b
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-06-18 02:35:24 -0700

    working on homepage

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Drawer from './components/Drawer';
 import AboutComponent from './components/About';
+import HomeComponent from './components/Home';
+
 
 function App() {
   return (
     <Drawer>
       <Switch>
         <Route path='/about' component={AboutComponent} />
-
+        <Route path='/' component={HomeComponent} />
       </Switch>
     </Drawer>
   );
