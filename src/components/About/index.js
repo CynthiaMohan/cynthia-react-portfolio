a1ae716ffe8aa4b661b4f9075d16f98cd630d6aa
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-06-18 02:34:42 -0700

    added about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,18 @@
+import Typography from '@material-ui/core/Typography';
+
 const About = () => {
     return (
-        <h1>About</h1>
+        <>
+            <h1>About</h1>
+            <Typography paragraph>
+                Hi I'm Cynthia Mohan.<br />
+                I am a Full Stack Developer holding a Masters in Computer and Communication Engineering.<br />
+                I recently earned a certificate in Full Stack Developmentfrom UC Berkeley where I was trained in HTML CSS and Javascript, jQuery moment.js and other libraries associated with javascript Nodejs Vuejs and more.<br />
+                I have good communication skills and bring to the project the ability to work independantly and as within a team. I focus on writing clean and efficient code.<br />
+                I love Photography, gardening, watching Netflix and spending time with my family.
+            </Typography>
+
+        </>
     );
 }
 
