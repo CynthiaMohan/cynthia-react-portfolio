fffce3ba06484ef382220183fec97efb25df6dbe
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-06-19 00:03:08 -0700

    working on contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import { FormControl, Input, InputLabel, FormHelperText, TextField, makeStyles } from '@material-ui/core';
 
-const Contact = () => {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        },
+    },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: '25ch',
+    },
+}));
+
+export default function ContactForm() {
+    const classes = useStyles();
     return (
-        <h1>Contact Page</h1>
+        <div>
+            <h1>Contact Page</h1>
+            <form className={classes.root} noValidate autoComplete="off">
+                <div>
+                    <TextField
+                        required
+                        id="name"
+                        label="Name"
+                        defaultValue=""
+                        placeholder="Enter your name here."
+                        variant="outlined"
+                    />
+
+                    <TextField
+                        required
+                        id="email"
+                        label="Email"
+                        defaultValue=""
+                        placeholder="Enter your email here."
+                        variant="outlined"
+                    />
+
+                    <TextField
+                        required
+                        id="message"
+                        label="Message"
+                        defaultValue=""
+                        placeholder="Enter your message here."
+                        multiline
+                        rows={4}
+                        variant="outlined"
+                    />
+
+                </div>
+            </form>
+        </div>
     );
 }
-
-export default Contact;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+
 const Home = () => {
     return (
-        <h1>Home Page</h1>
+        <div>
+            <h1>Home Page</h1>
+        </div>
     );
 };
 export default Home;
