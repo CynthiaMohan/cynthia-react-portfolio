3b3443474280d21d7b1773ad8703b66b745b819c
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-06-23 09:47:55 -0700

    Updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Drawer from './components/Drawer';
 import AboutComponent from './components/About';
 import WorkComponent from './components/Work';
@@ -9,17 +9,18 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Drawer>
-      <Switch>
-
-        <Route path='/about' component={AboutComponent} />
-        <Route path='/portfolio' component={WorkComponent} />
-        <Route path='/contact' component={ContactComponent} />
-        <Route path='/resume' component={ResumeComponent} />
-        <Route path='/' component={AboutComponent} />
-      </Switch>
-      <Footer />
-    </Drawer>
+    <HashRouter basename='/'>
+      <Drawer>
+        <Switch>
+          <Route path='/about' component={AboutComponent} />
+          <Route path='/portfolio' component={WorkComponent} />
+          <Route path='/contact' component={ContactComponent} />
+          <Route path='/resume' component={ResumeComponent} />
+          <Route path='/' component={AboutComponent} />
+        </Switch>
+        <Footer />
+      </Drawer>
+    </HashRouter>
   );
 };
 
