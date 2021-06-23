da6f21857752f5d1e1784fd5d112762bdd1df37a
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-06-23 02:50:00 -0700

    Added skills set to Resume along with link to download Resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,11 +1,68 @@
 import React from 'react';
-import skills from './skills';
-const Resume = () => {
+import { loadCSS } from 'fg-loadcss';
+import { Grid, makeStyles } from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
+import Resume from '../../assets/resume/Cynthia Mohan.pdf';
+import Icon from '@material-ui/core/Icon';
+import Tooltip from '@material-ui/core/Tooltip';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+        '& > .fa': {
+            margin: theme.spacing(2),
+        },
+        fab: {
+            margin: theme.spacing(2),
+        },
+        absolute: {
+            position: 'absolute',
+            bottom: theme.spacing(2),
+            right: theme.spacing(3),
+        },
+    },
+}));
+
+export default function ImageAvatars() {
+    const classes = useStyles();
+
+    React.useEffect(() => {
+        const node = loadCSS(
+            'https://use.fontawesome.com/releases/v5.12.0/css/all.css',
+            document.querySelector('#font-awesome-css'),
+        );
+
+        return () => {
+            node.parentNode.removeChild(node);
+        };
+    }, []);
+
     return (
         <>
             <h1>Skills</h1>
-            <skills></skills>
+            <div className={classes.root}>
+                <Tooltip title="HTML5">
+                    <Icon className="fab fa-html5" style={{ fontSize: 40 }} />
+                </Tooltip>
+                <Tooltip title="CSS3">
+                    <Icon className="fab fa-css3-alt" style={{ fontSize: 40 }} />
+                </Tooltip>
+                <Tooltip title="JavaScript">
+                    <Icon className="fab fa-js-square" style={{ fontSize: 40 }} />
+                </Tooltip>
+                <Tooltip title="Node js">
+                    <Icon className="fab fa-node-js" style={{ fontSize: 40 }} />
+                </Tooltip>
+                <Tooltip title="React js">
+                    <Icon className="fab fa-react" style={{ fontSize: 40 }} />
+                </Tooltip>
+
+
+            </div>
+            <h2><a href={Resume}>Download my Resume here</a></h2>
         </>
     );
-};
-export default Resume;
+}
