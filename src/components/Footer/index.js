034da51ae239dedda685397fb6d0a13363cd6106
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-06-23 09:25:49 -0700

    updates to resume

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,8 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
-import { white } from '@material-ui/core/colors';
+import EmailIcon from '@material-ui/icons/Email';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const useStyles = makeStyles({
     root: {
@@ -26,6 +27,9 @@ const useStyles = makeStyles({
         left: 0,
         justifyContent: "center"
     },
+    label: {
+        color: "#FFFFFF"
+    }
 });
 
 export default function SimpleBottomNavigation() {
@@ -43,15 +47,27 @@ export default function SimpleBottomNavigation() {
                 className={classes.root}
             >
                 <BottomNavigationAction
+                    className={classes.label}
                     label="Github"
                     icon={<GitHubIcon style={{ color: "#ffffff" }} />}
                     href={"https://github.com/CynthiaMohan"} />
-                <BottomNavigationAction label="LinkedIn" icon={<LinkedInIcon style={{ color: "#ffffff" }} />} href={"https://www.linkedin.com/in/cynthia-mohan/"} />
-                <BottomNavigationAction
-                    label="Phone"
-                    icon={<PhoneIphoneIcon style={{ color: "#ffffff" }}
-                    />} href={"tel:+12678442523"}
-                />
+                <BottomNavigationAction className={classes.label} label="LinkedIn" icon={<LinkedInIcon style={{ color: "#ffffff" }} />} href={"https://www.linkedin.com/in/cynthia-mohan/"} />
+                <Tooltip title="+1-267-844-2523">
+                    <BottomNavigationAction
+                        className={classes.label}
+                        label="Phone"
+                        icon={<PhoneIphoneIcon style={{ color: "#ffffff" }}
+                        />} href={"tel:+12678442523"}
+                    />
+                </Tooltip>
+                <Tooltip title="cynthiamohan1@gmail.com">
+                    <BottomNavigationAction
+                        className={classes.label}
+                        label="Email"
+                        icon={<EmailIcon style={{ color: "#ffffff" }}
+                        />} href={"mailto:cynthiamohan1@gmail.com"}
+                    />
+                </Tooltip>
             </BottomNavigation>
             <BottomNavigation className={classes.root2}> <p>Made by Cynthia Mohan 2021</p></BottomNavigation>
 
